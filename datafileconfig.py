8bdf7e098a45ecce5c1b089b7a6e0a98bcfd0786
Morris E Moe <31656736+morrise@users.noreply.github.com>
2024-04-08 10:11:54 +0800

    Update datafileconfig.py
    correct spelling from contact to concat

--- datafileconfig.py
+++ datafileconfig.py
@@ -377,7 +377,7 @@ def checkdata(checkcols, refcolumn, thislimit, thisdata):
         else: 
             #print("check pass 2")
             resultdata = resultdata.join(newdata)
-    faildata = pd.contact(tmpfaildata)
+    faildata = pd.concat(tmpfaildata)
     #print(resultdata)
     finalresult = resultdata.copy()
     finalresult = finalresult.replace(np.nan, True)
